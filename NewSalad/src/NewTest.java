4abe5f0495d92dc3d404345bc70b6a521845ca7a
shahi <shahi@192.168.0.14>
2018-05-29 01:34:28 -0400

    Commit 2018

---
+++ NewSalad/src/NewTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class NewTest {
+	
+	@Test
+	public void Apple() {
+		
+		System.out.println("I like apple phone");
+	}
+
+}
---
+++ NewSalad/src/TestFar.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class TestFar {
+	
+	@Test
+	public void Cary() {
+		
+		System.out.println("I like my home in Cary");
+	}
+
+}
