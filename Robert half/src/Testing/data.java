9271fef893fcfb30a4952f9a1b5ed6fce0f9481c
Mota2 <Mota2@LAPTOP-HGNOPU71>
2018-12-19 01:28:27 -0500

    New Farrukh Commit

---
+++ Robert half/src/Testing/data.java
@@ -0,0 +1,14 @@
+package Testing;
+
+class data {
+
+	public static void main(String[] args) {
+		String Name; 
+		Name = "Ayesha";
+		String LastName;
+		LastName ="Omair";
+		System.out.println("Name is " +  Name + ' '+ LastName);
+ 
+	}
+
+}
