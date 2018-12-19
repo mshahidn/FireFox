46063cb1202e8c832ca5b45d9cd4b1109c74d2ef
Mota2 <Mota2@LAPTOP-HGNOPU71>
2018-12-19 01:22:42 -0500

    Farrukh Commit

---
+++ Robert half/src/SeleniumTest/SystemFailure.java
@@ -0,0 +1,21 @@
+package SeleniumTest;
+
+public class SystemFailure {
+
+	public static void main(String[] args) {
+		int Num;
+		Num = 50;
+		int Num2;
+		Num2 = 40 ;
+		
+		int Total;
+		Total = Num  + Num2;
+		
+  System.out.println(" Total is " +Total);
+  
+  
+		   
+	}
+	
+
+}
